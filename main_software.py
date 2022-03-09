997c30f161d74e44b871aaedf227511dec981fcb
Álvaro <76467541+AlvaroMontiel@users.noreply.github.com>
2022-03-09 03:06:24 -0300

    Main (#7)
    * Se modificó igualar tablas, ajustando la tabular 4
    
    * pruebas del main_software.py
    
    Co-authored-by: AlvaroMontiel <alvaro,m.a@outlook.es>

--- main_software.py
+++ main_software.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from codigo_main import tablas_latex
 import os
-
+print("********")
 path = os.path.abspath(
        "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/DEF_1990_2018.csv")
 path2 = os.path.abspath(
        "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/estimaciones-y-proyecciones-2002-2035-comunas.xlsx")
-
+print("Cargando el dataset de defunciones...")
 df1 = pd.read_csv(path, sep=";", encoding='Latin',
                   dtype={21: str, 36: str, 37: str, 38: str, 39: str,
                          40: str, 41: str, 42: str, 43: str, 44: str,
@@ -16,14 +16,21 @@ df1 = pd.read_csv(path, sep=";", encoding='Latin',
                          82: str, 86: str, 89: str, 90: str, 91: str,
                          93: str, 96: str, 97: str, 98: str, 99: str,
                          100: str})
-
+print("Dataframe de defunciones cargado")
+print("********")
+print("Cargando el dataset de población...")
 df2 = pd.read_excel(path2)
-print('Data frames cargados')
-tabular = 2
+print("Dataframe de población cargado")
+print("********")
+print('Cargando variables')
+tabular = 4
 sexo = 0
-region = 0
-comuna = 2101
-periodo = "2014-2018"
+region = 2
+comuna = 0
+periodo = "2011-2015"
 print('Variables cargadas')
+print("********")
+print('Ejecutando función...')
 a = tablas_latex.crear_tablas(df1, df2, periodo=periodo, tabular=tabular, sexo=sexo, comuna=comuna, region=region, edad_quinquenal = False)
 print(f"Tabla para: \nlista tabular: {tabular}, \nregion: {region}, \ncomuna: {comuna}, \nsexo: {sexo}, \nperiodo: {periodo} \n {a}")
+print('Fin del proceso')
