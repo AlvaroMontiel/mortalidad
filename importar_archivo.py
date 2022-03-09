8b83bcb3009a381192fbe6c43b4077ca63d611d9
AlvaroMontiel <alvaro,m.a@outlook.es>
2022-03-09 11:51:17 -0300

    Se modificó la clase ImportarArchivo.py, ahora basta con instancia un solo objeto y este almacena los dataframes como atributos

--- importar_archivo.py
+++ importar_archivo.py
@@ -1,34 +1,42 @@
 class ImportarArchivo:
-    def __init__(self, extension, ruta):
-        self.__extension = extension
-        self.__ruta = ruta
-        self.dataframe = self.__importar_dataset()
+    def __init__(self, ruta_defunciones, ruta_poblacion):
+        self.__ruta_defunciones = ruta_defunciones
+        self.__ruta_poblacion = ruta_poblacion
+        self.poblacion = ImportarArchivo.importar_dataset_poblacion(self)
+        self.defunciones = ImportarArchivo.importar_dataset_defunciones(self)
 
-    def __importar_dataset(self):
+    def importar_dataset_poblacion(self):
         import pandas as pd
         import os
-        path = os.path.abspath(self.__ruta)
+        path_poblacion = os.path.abspath(self.__ruta_poblacion)
 
-        if self.__extension == "xlsx":
-            df = pd.read_excel(path)
-        elif self.__extension == "csv":
-            df = pd.read_csv(path, sep=';', encoding='Latin',
-                             dtype={21: str, 36: str, 37: str, 38: str, 39: str,
-                                    40: str, 41: str, 42: str, 43: str, 44: str,
-                                    45: str, 46: str, 47: str, 48: str, 49: str,
-                                    50: str, 58: str, 60: str, 64: str, 67: str,
-                                    69: str, 70: str, 71: str, 73: str, 76: str,
-                                    82: str, 86: str, 89: str, 90: str, 91: str,
-                                    93: str, 96: str, 97: str, 98: str, 99: str,
-                                    100: str})
-        else:
-            print("El archivo a importar debe tener extensión xlsx o csv")
+        df_poblacion = pd.read_excel(path_poblacion)
 
-        return df
+        return df_poblacion
+
+    def importar_dataset_defunciones(self):
+        import pandas as pd
+        import os
+
+        path_defunciones = os.path.abspath(self.__ruta_defunciones)
+
+        df_defunciones = pd.read_csv(path_defunciones, sep=';', encoding='Latin',
+                                     dtype={21: str, 36: str, 37: str, 38: str, 39: str,
+                                            40: str, 41: str, 42: str, 43: str, 44: str,
+                                            45: str, 46: str, 47: str, 48: str, 49: str,
+                                            50: str, 58: str, 60: str, 64: str, 67: str,
+                                            69: str, 70: str, 71: str, 73: str, 76: str,
+                                            82: str, 86: str, 89: str, 90: str, 91: str,
+                                            93: str, 96: str, 97: str, 98: str, 99: str,
+                                            100: str})
+
+        return df_defunciones
 
 
 if __name__ == "__main__":
-    poblacion = ImportarArchivo("xlsx", "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/estimaciones-y-proyecciones-2002-2035-comunas.xlsx")
-    defunciones = ImportarArchivo("csv", "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/DEF_2010_2018.csv")
-    print(poblacion.dataframe.shape)
-    print(defunciones.dataframe.shape)
+# Creación del objeto ImportarArchivo
+    datos = ImportarArchivo("/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/DEF_2010_2018.csv",
+                            "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/poblacion_corto.xlsx")
+
+    print(datos.poblacion.shape
+    print(datos.defunciones.shape)
--- parametros_calculos.py
+++ parametros_calculos.py
@@ -7,29 +7,41 @@ class ParametrosCalculos(ImportarArchivo):
     El método importar data set es heredado de la clase ImportarArchivo.py y sirve para crear los objetos que contienen
     los dataframes de poblacion y de defunciones
     '''
-    def __init__(self, extension, ruta, periodo, sexo=None, region=None, comuna=None, edad=False):
+    def __init__(self, extension, ruta, periodo, enfermedad, categoria_enfermedad, sexo=None, region=None, comuna=None, edad=False):
         super().__init__(extension, ruta)
         self.periodo = periodo
+        self.enfermedad = enfermedad
+        self.categoria_enfermedad = categoria_enfermedad
         self.sexo = sexo
         self.region = region
         self.comuna = comuna
         self.edad = edad
 
-    def __importar_dataset(self):
-        super().__importar_dataset()
 
-    def aplicar_periodo_defuncion(self):
-        super().__importar_dataset()
+    def filtrar_dataframe(self):
+        defunciones = ImportarArchivo("csv", self.ruta)
+        poblacion = ImportarArchivo("xlsx", self.ruta)
+        if self.extension == "csv": #dataset de mortalidad
+            pass
+        elif self.extension == "xlsx": #dataset de poblacion
+            pass
+
+        if self.sexo == None or self.region == None or self.comuna == None or self.edad == False:
+            return print(self.dataframe.shape)
+
+
+    def separa_edad(self):
+        pass
 
 
 
 if __name__ == "__main__":
     poblacion = ParametrosCalculos("xlsx",
                                 "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/poblacion_corto.xlsx",
-                                   "2014-2018")
-    defunciones = ParametrosCalculos("csv",
-                                  "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/DEF_2010_2018.csv",
-                                     "2014-2018")
-    print(poblacion.dataframe.shape)
-    print(defunciones.dataframe.shape)
-
+                                   "2014-2018", "cancer", 1)
+    print(poblacion.importar_dataset().shape)
+#    defunciones = ParametrosCalculos("csv",
+#                                  "/Users/alvaro/Documents/Data_Science/Software_mortalidad/datasets/DEF_2010_2018.csv",
+#                                     "2014-2018")
+#
+#    print(defunciones.dataframe.shape)
